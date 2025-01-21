31b9d6dd6defe17fe3c2bf8cac22c2bf9a09ea99
Paul Talaga <talagap@uindy.edu>
2025-01-21 16:11:05 -0500

    Quote fix for sh

--- LAMP-Starter/startDocker.sh
+++ LAMP-Starter/startDocker.sh
@@ -2,4 +2,4 @@
 # For fedora SE linux: sudo setenforce Permissive
 mkdir mysql
 mkdir logs
-docker run -i --rm --platform linux/amd64 -t -p 80:80 -p 3306:3306 -v ${PWD}/app:/app -v ${PWD}/mysql:/var/lib/mysql -v ${PWD}/logs:/var/log/apache2 mattrayner/lamp:0.8.0-1804-php8
+docker run -i --rm --platform linux/amd64 -t -p 80:80 -p 3306:3306 -v "${PWD}/app:/app" -v "${PWD}/mysql:/var/lib/mysql" -v "${PWD}/logs:/var/log/apache2" mattrayner/lamp:0.8.0-1804-php8
